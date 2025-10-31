b5d2281ffc04a6390122f5551edc250106a7812c
safiya <sksafiya484@gmail.com>
2025-10-31 12:10:14 +0530

    changes

---
+++ .source/index.ts
@@ -0,0 +1,6 @@
+// @ts-nocheck -- skip type checking
+import * as docs_1 from "../content/docs/Intro.mdx?collection=docs"
+import * as docs_0 from "../content/docs/find-duplicates-in-array.mdx?collection=docs"
+import { _runtime } from "fumadocs-mdx/runtime/next"
+import * as _source from "../source.config"
+export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"find-duplicates-in-array.mdx","fullPath":"content\\docs\\find-duplicates-in-array.mdx"}, data: docs_0 }, { info: {"path":"Intro.mdx","fullPath":"content\\docs\\Intro.mdx"}, data: docs_1 }], [])
