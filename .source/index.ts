d47560141f4e0f488e89ebac7c804e6bf385daf8
safiya <sksafiya484@gmail.com>
2025-11-03 11:22:28 +0530

    changes

--- .source/index.ts
+++ .source/index.ts
@@ -1,6 +1,8 @@
 // @ts-nocheck -- skip type checking
-import * as docs_1 from "../content/docs/Intro.mdx?collection=docs"
+import * as docs_3 from "../content/docs/K Closest Points to Origin.mdx?collection=docs"
+import * as docs_2 from "../content/docs/Intro.mdx?collection=docs"
+import * as docs_1 from "../content/docs/find-missing-number-in-array.mdx?collection=docs"
 import * as docs_0 from "../content/docs/find-duplicates-in-array.mdx?collection=docs"
 import { _runtime } from "fumadocs-mdx/runtime/next"
 import * as _source from "../source.config"
-export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"find-duplicates-in-array.mdx","fullPath":"content\\docs\\find-duplicates-in-array.mdx"}, data: docs_0 }, { info: {"path":"Intro.mdx","fullPath":"content\\docs\\Intro.mdx"}, data: docs_1 }], [])
+export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"find-duplicates-in-array.mdx","fullPath":"content\\docs\\find-duplicates-in-array.mdx"}, data: docs_0 }, { info: {"path":"find-missing-number-in-array.mdx","fullPath":"content\\docs\\find-missing-number-in-array.mdx"}, data: docs_1 }, { info: {"path":"Intro.mdx","fullPath":"content\\docs\\Intro.mdx"}, data: docs_2 }, { info: {"path":"K Closest Points to Origin.mdx","fullPath":"content\\docs\\K Closest Points to Origin.mdx"}, data: docs_3 }], [])
